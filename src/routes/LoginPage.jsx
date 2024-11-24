16ad5d7e1ec121ce10be55aff7d1dfc5abc7583c
Hemanth <hemanthks2000@gmail.com>
2024-11-24 22:47:23 +0530

    create routes

---
+++ src/routes/LoginPage.jsx
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+  return (
+    <div className='bg-red-400'>LoginPage</div>
+  )
+}
+
+export default LoginPage
---
+++ src/routes/PostListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostListPage = () => {
+  return (
+    <div className='bg-red-400'>PostListPage</div>
+  )
+}
+
+export default PostListPage
