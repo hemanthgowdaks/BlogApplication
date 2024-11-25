b329d1c5ca9e72e35a8570afa035c42206d48c4b
Hemanth <hemanthks2000@gmail.com>
2024-11-25 22:26:30 +0530

    routing to different pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,77 @@
-import { useState } from "react"
+import { useState } from "react";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [open,setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="h-16 md:h-20 flex items-center justify-between">
-        {/* logo */}
-        <div className="flex items-center gap-4 text-2xl font-bold">
-            <Image src="logo.png" alt="GrowthAlgo Logo" w={32} h={32} />
-            <span>Growth Algo</span>
+      {/* logo */}
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
+        <Image src="logo.png" alt="GrowthAlgo Logo" w={32} h={32} />
+        <span>Growth Algo</span>
+      </Link>
+      {/* mobile menu */}
+      <div className="md:hidden">
+        {/* MOBILE BUTTON */}
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {/* Change Hamburger Icon */}
+          {/* {open ? "X" : "☰"} */}
+          <div className="flex flex-col gap-[5.4px]">
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
+                open && "rotate-45"
+              }`}
+            ></div>
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black transition-all ease-in-out ${
+                open && "opacity-0"
+              }`}
+            ></div>
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
+                open && "-rotate-45"
+              }`}
+            ></div>
+          </div>
         </div>
-        {/* mobile menu */}
-        <div className="md:hidden">
-            {/* mobile button */}
-            <div onClick={()=>setOpen((prev)=>!prev)} 
-            className="text-4xl cursor-pointer">
-                {open ? "X" : "☰"}
-            </div>
-            {/* mobile link list */}
-            
-            <div className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
-            open ? "-right-0" : "-right-[100%]"}`}>
-            <a href="/">Home</a>
-            <a href="/">Trending</a>
-            <a href="/">Most Popular</a>
-            <a href="/">About</a>
-            <a href="/">
-                <button className="bg-blue-800 px-8 py-2 rounded-3xl text-white">Login</button>
-            </a>
-            </div>
-        </div>
-        
-        {/* desktop menu */}
-        <div className="hidden md:flex items-center gap-4 xl:gap-8 font-medium">
-            <a href="/">Home</a>
-            <a href="/">Trending</a>
-            <a href="/">Most Popular</a>
-            <a href="/">About</a>
-            <a href="/">
-                <button className="bg-blue-800 px-8 py-2 rounded-3xl text-white">Login</button>
-            </a>
-        </div>
-    </div>
-  )
-}
+        {/* mobile link list */}
 
-export default Navbar
+        <div
+          className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
+            <button className="bg-blue-800 px-8 py-2 rounded-3xl text-white">
+              Login
+            </button>
+          </Link>
+        </div>
+      </div>
+
+      {/* desktop menu */}
+      <div className="hidden md:flex items-center gap-4 xl:gap-8 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="bg-blue-800 px-8 py-2 rounded-3xl text-white">
+            Login
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
