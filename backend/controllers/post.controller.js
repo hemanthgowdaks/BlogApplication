acbf505324a3f835027e501d71033375245a834e
Hemanth <hemanthks2000@gmail.com>
2024-12-01 22:35:57 +0530

    clerk middleware

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import Post from "../models/post.model.js"
+import User from "../models/user.model.js"
 
 export const getPosts = async (req, res) =>{
     const posts = await Post.find();
@@ -11,7 +12,18 @@ export const getPost = async (req, res) =>{
 }
 
 export const createPost = async(req,res) => {
-    const newPost = new Post(req.body);
+
+    const clerkUserId = req.auth.userId;
+    if(!clerkUserId){
+        return res.status(401).json("Not authenticated")
+    }
+
+    const user = await User.findOne({clerkUserId})
+    if(!user){
+        return res.status(404).json("User not found")
+    }
+
+    const newPost = new Post({user:user._id, ...req.body});
     const post = await newPost.save();
     res.status(200).json(post)
 }
--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,24 @@ import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import webhookRouter from "./routes/webhook.route.js";
+import { clerkMiddleware,requireAuth } from '@clerk/express'
 
 
 const app = express();
+app.use(clerkMiddleware())
 const PORT = process.env.PORT || 5000;
 app.use("/webhooks", webhookRouter)
 app.use(express.json());
 
-// app.use((error, req, res, next) => {
-//   res.status(error.status || 500);
-//   res.json({
-//     message: error.message || "Somthing went wrong!",
-//     status: error.status,
-//     stack: error.stack,
-//   });
-// });
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message || "Somthing went wrong!",
+    status: error.status,
+    stack: error.stack,
+  });
+});
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
